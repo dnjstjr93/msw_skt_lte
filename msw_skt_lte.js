6bbcfa7f04383b9956e44c39ff4560daa5b9ed99
dnjstjr93 <dnjstjr93@gmail.com>
2020-11-02 17:53:56 +0900

    - edit topic for mission data upload to each sortie name

--- msw_skt_lte.js
+++ msw_skt_lte.js
@@ -241,12 +241,7 @@ function parseDataMission(topic, str_message) {
         var topic_arr = topic.split('/');
         var data_topic = '/Mobius/' + config.gcs + '/Mission_Data/' + config.drone + '/' + config.name + '/' + topic_arr[topic_arr.length-1];
         msw_mqtt_client.publish(data_topic + '/' + my_sortie_name, str_message);
-        console.log(my_sortie_name);
-        console.log(data_topic + config.sortie_name);
-        // /Mobius/KETI_MUV/Mission_Data/LTE_SKT/msw_skt_lte/LTE/disarm
-
         // msw_mqtt_client.publish(data_topic, str_message);
-        // /Mobius/KETI_MUV/Mission_Data/LTE_SKT/msw_skt_lte/LTE
     }
     catch (e) {
         console.log('[parseDataMission] data format of lib is not json');
